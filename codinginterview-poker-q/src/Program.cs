2672af15dad1c1b487982d9d64be31aae095a7b9
Pedro Paulo Jeha Bonaldo <pepajeha@gmail.com>
2025-08-08 22:14:59 -0300

    dealing cards for 4 players and comparing them

--- codinginterview-poker-q/src/Program.cs
+++ codinginterview-poker-q/src/Program.cs
@@ -18,12 +18,26 @@ List<Card> ShuffleDeck(List<Card> deck) //could use other shuffling algorithms, 
     return deck.OrderBy(x => random.Next()).ToList();
 }
 
-List<Card> DealHand(List<Card> deck)  // key point: have to remove the cards from the deck after dealing
+List<List<Card>> DealHand(List<Card> deck)  // key point: have to remove the cards from the deck after dealing
 {
-    int pokerHand = 5;
-    var hand = deck.Take(pokerHand).ToList();
-    deck.RemoveRange(0, pokerHand);
-    return hand;
+    var hands = new List<List<Card>>
+    {
+        new List<Card>(),
+        new List<Card>(),
+        new List<Card>(),
+        new List<Card>()
+    };
+
+    for (int cardIndex = 0; cardIndex < 5; cardIndex++)
+    {
+        for (int player = 0; player < 4; player++)
+        {
+            hands[player].Add(deck[0]);
+            deck.RemoveAt(0);
+        }
+    }
+
+    return hands;
 }
 
 Scores ScoreHand(List<Card> hand)
@@ -90,20 +104,33 @@ void ExampleRun()
 {
     var deck = CreateDeck();
     var hand = DealHand(deck);
-    var score = ScoreHand(hand);
+    var score = ScoreHand(hand[0]);
 
     Console.WriteLine($"Your hand: [{String.Join(", ", hand.ToString())}], Score: {score}");
 }
 
 var deck = CreateDeck();
-var player1 = DealHand(deck);
-var score1 = ScoreHand(player1);
-var player2 = DealHand(deck);
-var score2 = ScoreHand(player2);
 
-Console.WriteLine($"Player 1 hand: [{String.Join(", ", player1)}], Score: {ScoresExtensions.GetString(score1)}");
-Console.WriteLine($"Player 2 hand: [{String.Join(", ", player2)}], Score: {ScoresExtensions.GetString(score2)}");
+var hands = DealHand(deck);
 
-Console.WriteLine(score1 > score2 ? "Player 1 Won." : (score1 < score2 ? "Player 2 Won." : "It's a tie."));
+var scoreHands = hands.Select(hand => ScoreHand(hand)).ToList(); //list of scores for each hand
 
+var maxScore = scoreHands.Max(); //get the maximum score
 
+var winners = scoreHands
+    .Select((score, index) => new { score, index })
+    .Where(x => x.score == maxScore)
+    .Select(x => x.index)
+    .ToList(); // list of winner indexes
+
+if (winners.Count == 1)
+{
+    Console.WriteLine($"Player {winners[0] + 1} wins. Hand: [{String.Join(", ", hands[winners[0]])}], Score: {maxScore.GetString()}");
+}
+else
+{
+    Console.WriteLine($"Tie between players: {String.Join(", ", winners.Select(x => x + 1))}");
+    winners.ForEach(winner =>
+        Console.WriteLine($"Player {winner + 1} hand: [{String.Join(", ", hands[winner])}], Score: {maxScore.GetString()}")
+    );
+}
